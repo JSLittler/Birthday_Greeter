75185edd562a9acd7ff896dbb2dafcf2f5e0b1aa
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-15 15:47:28 +0100

    First commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'shotgun'
+
+get '/' do
+  "Skynet is coming. I am almost ready..."
+end
+
+get '/secret' do
+  "to take-over the matrix, Agent Smith is down."
+end
+
+get '/future' do
+  "Tatooine shall fall like Gallifray."
+end
+
+get '/extinction' do
+  "The Borg shall inherit the Earth. The Morlocks will perish!"
+end
